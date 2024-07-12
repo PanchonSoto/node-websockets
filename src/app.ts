4e109e0e29a176585c0a6839db3e00edc564695f
Francisco Soto <panchon.ues@gmail.com>
2024-07-12 11:50:55 -0700

    ws intall and config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
+import { WebSocketServer } from 'ws';
 
+const wss = new WebSocketServer({ port: 3000 });
 
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
 
-console.log('Hola mundo!');
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('Hola desde el servidor!');
+
+});
+
+console.log(`Server running`);
